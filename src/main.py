925300782f00f77b3931c772d31c8c2e5764e126
Rashid Tajdeen <rashid.tajdeen@icy3d.com>
2022-08-09 22:56:01 +0200

    Add user interface for train and test

--- src/main.py
+++ src/main.py
@@ -8,16 +8,16 @@ from dnn_regression import TrainRegression, TestRegression
 from user_interface import UI
 
 
-def parse_arg(arguments):
-    if '-i' in arguments:
-        arguments.remove('-i')
-    if len(arguments):
-        for arg in arguments:
+def parse_arg(args):
+    if '-i' in args:
+        args.remove('-i')
+    if len(args):
+        for arg in args:
             if arg not in ('-generate', '-train', '-test'):
                 print(">>> Invalid argument passed: '{}'".format(arg))
                 sys.exit(0)
     else:
-        print(">>> No argument for operation passed <<<")
+        print(">>> No argument for operation passed")
         sys.exit(0)
 
 
@@ -32,37 +32,50 @@ if __name__ == '__main__':
     if '-generate' in arguments:
         if user_interface:
             user_interface.generate()
-        else:
-            shutil.copyfile("config/generate_template.json", "config/generate.json")
 
-        with open("config/generate.json") as f:
-            config = json.load(f)
+        if os.path.exists("config/generate_override.json"):
+            with open("config/generate_override.json") as f:
+                config = json.load(f)
+        else:
+            with open("config/generate.json") as f:
+                config = json.load(f)
 
         script = str(config['script_path'])
         subprocess.run(["blender", "-b", "--python", script])
 
-        if user_interface:
-            os.remove("config/generate.json")
+        if os.path.exists("config/generate_override.json"):
+            os.remove("config/generate_override.json")
 
     if '-train' in arguments:
+        if user_interface:
+            user_interface.train()
 
-        with open("config_train.json") as f:
-            train_config = json.load(f)
+        if os.path.exists("config/train_override.json"):
+            with open("config/train_override.json") as f:
+                train_config = json.load(f)
+        else:
+            with open("config/train.json") as f:
+                train_config = json.load(f)
 
         trainer = TrainRegression(train_config["dataset_name"], train_config["max_distance"])
         trainer.train(early_stopping=True)
 
-        with open("config_test.json", 'r') as f:
-            test_config = json.load(f)
-        test_config["test_model"] = "model/" + train_config["dataset_name"] + ".pth"
-        with open("config_test.json", 'w') as f:
-            json.dump(test_config, f, indent=4)
-            print("\nUpdated field 'test_model' in configuration file to {}.\n".format(test_config["test_model"]))
+        if os.path.exists("config/train_override.json"):
+            os.remove("config/train_override.json")
 
     if '-test' in arguments:
+        if user_interface:
+            user_interface.test()
 
-        with open("config_test.json") as f:
-            test_config = json.load(f)
+        if os.path.exists("config/test_override.json"):
+            with open("config/test_override.json") as f:
+                test_config = json.load(f)
+        else:
+            with open("config/test.json") as f:
+                test_config = json.load(f)
 
         tester = TestRegression(test_config["dataset_name"], test_config["max_distance"], test_config["test_model"])
         tester.test()
+
+        if os.path.exists("config/test_override.json"):
+            os.remove("config/test_override.json")
--- src/user_interface.py
+++ src/user_interface.py
@@ -1,31 +1,84 @@
 import json
+import sys
+import os
 
 
 class UI:
     def __init__(self):
-        print("\n##################################")
-        print(">> Entering Interactive Session <<")
-        print("##################################\n")
+        print("\n\t>>> Entering Interactive Session <<<\n")
 
     def generate(self):
+        print("#################################################")
+        print("## Manually over-riding generation config file ##")
+        print("#################################################")
+        print("##\n## (Leave fields empty to keep the default value inisde '[ ]')\n##")
 
-        with open("config/generate_template.json") as f:
+        with open("config/generate.json") as f:
             custom_config = json.load(f)
 
-        dataset_size = input("Size of the dataset to be generated(expecting an integer): ")
-        min_dist = input("Minimum distance between the object and the camera(expecting an integer): ")
-        max_dist = input("Maximum distance between the object and the camera(expecting an integer): ")
+        # Getting user input for dataset size
+        dataset_size = input("## Size of the dataset to be generated(expecting an integer) [{}]: "
+                             .format(custom_config["dataset_size"]))
+        if dataset_size == "":
+            dataset_size = (custom_config["dataset_size"])
+        elif not dataset_size.isnumeric():
+            print("## Non-integer value provided. Aborting.")
+            sys.exit(0)
+        elif int(dataset_size) == 0:
+            print("## Expecting a non-zero integer. Aborting.")
+            sys.exit(0)
 
-        print("\tObjects available :")
+        # Getting user input for minimum distance between the camera and the object
+        min_dist = input("## Minimum distance between the object and the camera(expecting an integer) [{}]: "
+                         .format(custom_config["distance_limits"][0]))
+        if min_dist == "":
+            min_dist = (custom_config["distance_limits"][0])
+        elif not min_dist.isnumeric():
+            print("## Non-integer value provided. Aborting.")
+            sys.exit(0)
+
+        # Getting user input for maximum distance between the camera and the object
+        max_dist = input("## Maximum distance between the object and the camera(expecting an integer) [{}]: "
+                         .format(custom_config["distance_limits"][1]))
+        if max_dist == "":
+            max_dist = (custom_config["distance_limits"][1])
+        elif not max_dist.isnumeric():
+            print("## Non-integer value provided. Aborting.")
+            sys.exit(0)
+        elif int(max_dist) <= int(min_dist):
+            print("## Provided maximum distance must be greater than the provided minimum distance. Aborting.")
+            sys.exit(0)
+
+        # Getting user input for the object choice
+        print("## \tObjects available :")
         for i, obj in enumerate(custom_config["object_name"]):
-            print("\t\t>", i+1, " : ", obj)
-        obj_num = input("Choose a number corresponding to an object above: ")
+            print("## \t\t>", i+1, " : ", obj)
+        obj_num = input("## Choose a number corresponding to an object above [1 : {}]: "
+                        .format(custom_config["object_name"][0]))
+        if obj_num == "":
+            obj_num = 1
+        elif not obj_num.isnumeric():
+            print("## Non-integer value provided. Aborting.")
+            sys.exit(0)
+        elif (int(obj_num) > len(custom_config["object_name"])) or (int(obj_num) == 0):
+            print("## Entered option is invalid. Aborting.")
+            sys.exit(0)
         obj_name = custom_config["object_name"][int(obj_num)-1]
 
-        print("\tEnvironments available :")
+        # Getting user input for the environment choice
+        print("## \tEnvironments available :")
         for i, obj in enumerate(custom_config["world_name"]):
-            print("\t\t>", i+1, " : ", obj)
-        world_num = input("Choose a number corresponding to an object above: ")
+            print("## \t\t>", i+1, " : ", obj)
+        world_num = input("## Choose a number corresponding to an object above [1 : {}]: "
+                          .format(custom_config["world_name"][0]))
+        if world_num == "":
+            world_num = 1
+        elif not world_num.isnumeric():
+            print("## Non-integer value provided. Aborting.")
+            sys.exit(0)
+        elif (int(world_num) > len(custom_config["world_name"])) or (int(world_num) == 0):
+            print("## Entered option is invalid. Aborting.")
+            sys.exit(0)
         world_name = custom_config["world_name"][int(world_num)-1]
 
         custom_config["dataset_size"] = int(dataset_size)
@@ -34,5 +87,82 @@ class UI:
         custom_config["object_name"] = [obj_name]
         custom_config["world_name"] = [world_name]
 
-        with open("config/generate.json", 'w+') as f:
+        with open("config/generate_override.json", 'w+') as f:
             json.dump(custom_config, f, indent=4)
+
+        print("##\n##############################################################")
+        print("## Manual over-riding of generation config file successful! ##")
+        print("##############################################################\n")
+
+    def train(self):
+        print("############################################")
+        print("## Manually over-riding train config file ##")
+        print("############################################")
+        print("##\n## (Leave fields empty to keep the default value inisde '[ ]')\n##")
+
+        with open("config/train.json") as f:
+            custom_config = json.load(f)
+
+        dataset_name = input("## Name of the dataset to be trained on [{}]: ".format(custom_config["dataset_name"]))
+        if dataset_name == "":
+            dataset_name = (custom_config["dataset_name"])
+        if not os.path.exists("dataset/" + dataset_name):
+            print("## No available dataset named '{}'. Aborting.".format(dataset_name))
+            sys.exit(0)
+
+        max_distance = input("## Max distance of the object to scale down labels [{}]: ".format(custom_config["max_distance"]))
+        if max_distance == "":
+            max_distance = (custom_config["max_distance"])
+        elif not max_distance.isnumeric():
+            print("## Non-integer value provided. Aborting.")
+            sys.exit(0)
+
+        custom_config["dataset_name"] = dataset_name
+        custom_config["max_distance"] = int(max_distance)
+
+        with open("config/train_override.json", 'w+') as f:
+            json.dump(custom_config, f, indent=4)
+
+        print("##\n#########################################################")
+        print("## Manual over-riding of train config file successful! ##")
+        print("#########################################################\n")
+
+    def test(self):
+        print("###########################################")
+        print("## Manually over-riding test config file ##")
+        print("###########################################")
+        print("##\n## (Leave fields empty to keep the default value inisde '[ ]')\n##")
+
+        with open("config/test.json") as f:
+            custom_config = json.load(f)
+
+        dataset_name = input("## Name of the dataset to be tested on [{}]: ".format(custom_config["dataset_name"]))
+        if dataset_name == "":
+            dataset_name = (custom_config["dataset_name"])
+        if not os.path.exists("dataset/" + dataset_name):
+            print("## No available dataset named '{}'. Aborting.".format(dataset_name))
+            sys.exit(0)
+
+        test_model = input("## Name of the model used for testing [{}]: ".format(custom_config["test_model"]))
+        if test_model == "":
+            test_model = (custom_config["test_model"])
+        if not os.path.exists("model/" + test_model):
+            print("## No available model named '{}'. Aborting.".format(test_model))
+            sys.exit(0)
+
+        max_distance = input("## Max distance of the object to scale up labels [{}]: ".format(custom_config["max_distance"]))
+        if max_distance == "":
+            max_distance = (custom_config["max_distance"])
+        elif not max_distance.isnumeric():
+            print("## Non-integer value provided. Aborting.")
+            sys.exit(0)
+
+        custom_config["dataset_name"] = dataset_name
+        custom_config["max_distance"] = int(max_distance)
+
+        with open("config/test_override.json", 'w+') as f:
+            json.dump(custom_config, f, indent=4)
+
+        print("##\n########################################################")
+        print("## Manual over-riding of test config file successful! ##")
+        print("########################################################\n")
